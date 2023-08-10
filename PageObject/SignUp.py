210f615aa176c2e0bb91a4e814186576c3b9879e
Afnan Zia <afnanzia74@gmail.com>
2023-08-11 00:09:00 +0500

    new changes By FS

--- PageObject/SignUp.py
+++ PageObject/SignUp.py
@@ -1,44 +1,37 @@
 import time
-
-from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import Select
 
-serv = Service("C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver")
-driver = webdriver.Chrome()
-# driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-wait = WebDriverWait(driver, 10)
+class Signup:
 
-
-class SignUp:
+    def __init__(self, driver):
+        self.driver = driver
 
     def open_signup_url(self):
-        driver.get("https://develop.somomarketingtech.com/select-platform")
-        driver.maximize_window()
+        self.driver.get("https://develop.somomarketingtech.com/select-platform")
+        self.driver.maximize_window()
+
+        # Assert that the title contains "Select Platform"
+        assert "Select Platform" in self.driver.title, "Signup URL did not open successfully"
 
     def scroll_windows(self):
-        driver.execute_script("window.scrollTo(0, 400)")
+        self.driver.execute_script("window.scrollTo(0, 400)")
 
-    def selectplatform_driver(self):
-        btn_driver = driver.find_element(By.CSS_SELECTOR, ".btn.color-secondary-bg.px-4")
-        driver.execute_script("arguments[0].click();", btn_driver)
+    def click_on_driver_button(self):
+        btn_driver = self.driver.find_element(By.CSS_SELECTOR, ".btn.color-secondary-bg.px-4")
+        self.driver.execute_script("arguments[0].click();", btn_driver)
 
     def selectplatform_advertiser(self):
-        SignUp.scroll_windows(self)
-        btn_advertiser = driver.find_elements(By.CSS_SELECTOR, ".btn.color-secondary-bg.px-4")
+        self.scroll_windows()
+        btn_advertiser = self.driver.find_elements(By.CSS_SELECTOR, ".btn.color-secondary-bg.px-4")
         btn_advertiser[1].click()
 
     def select_address(self):
-        business_address = driver.find_element(By.ID, "map")
+        business_address = self.driver.find_element(By.ID, "map")
         business_address.send_keys("nj clark")
-        suggestion_list = wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "pac-container")))
+        suggestion_list = WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "pac-container")))
 
         if len(suggestion_list) > 0:
             suggestion_list[0].click()  # Click on the first suggestion to select the address
@@ -46,94 +39,61 @@ class SignUp:
             # If no suggestions are found, you may need to handle this scenario differently
             print("No suggestions found.")
 
-    def phonenumber(self, value):
+    def enter_phone_number(self, value):
         phone_number = value
-        business_contact_no = driver.find_element(By.CSS_SELECTOR, ".vti__input")
+        business_contact_no = self.driver.find_element(By.CSS_SELECTOR, ".vti__input")
         business_contact_no.send_keys(phone_number)
 
-    def btn_continue(self, argument):
+    def enter_owner_address(self, value):
+        address = value
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".form-control.bg-transparent.myclass-event.pac-target-input")))
+        owner_address = self.driver.find_element(By.CSS_SELECTOR, ".form-control.bg-transparent.myclass-event.pac-target-input")
+        owner_address.send_keys(address)
+
+    def click_btn_continue(self, argument):
         argu = argument
-        btn_continue = driver.find_elements(By.CSS_SELECTOR, ".btn.button-primary")
+        btn_continue = self.driver.find_elements(By.CSS_SELECTOR, ".btn.button-primary")
         btn_continue[argu].click()
+
+    def verify_success_message(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body.x-layout.swal2-toast-shown.swal2-shown:nth-child(2) > div.swal2-container.swal2-top-end.swal2-backdrop-show:nth-child(8)")))
+        message= self.driver.find_element(By.CSS_SELECTOR, "body.x-layout.swal2-toast-shown.swal2-shown:nth-child(2) > div.swal2-container.swal2-top-end.swal2-backdrop-show:nth-child(8)").text
+        print(message)
+
+    def enter_password(self, password):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//wizard-button[contains(text(),'Next')]")))
+        password_input = self.driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Password"]').send_keys(password)
+
+    def enter_confirm_password(self, password):
+        confirm_password_input = self.driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Confirm Password"]').send_keys(password)
 
     def signup_names(self, argument, value):
         field_text = value
         argu = argument
-        name_field = driver.find_elements(By.CSS_SELECTOR, ".form-control.bg-transparent")
+        name_field = self.driver.find_elements(By.CSS_SELECTOR, ".form-control.bg-transparent")
+        name_field[argu].click()
         name_field[argu].send_keys(field_text)
 
     def drp_regtype_click(self):
-        drp_type = driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent")
-        drp_type.click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".form-select.bg-transparent"))).click()
+        #drp_type = self.driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent")
+        #drp_type.click()
 
     def drp_select_type(self):
-        sel_regtype = Select(driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent"))
+        sel_regtype = Select(self.driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent"))
         sel_regtype.select_by_index(1)
 
     def adv_dropdown(self, value):
         val = value
-        drp_advertising = driver.find_elements(By.CSS_SELECTOR, ".form-select.bg-transparent")
+        drp_advertising = self.driver.find_elements(By.CSS_SELECTOR, ".form-select.bg-transparent")
         drp_advertising[val].click()
-        selt_advertising = Select(driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent"))
+        selt_advertising = Select(self.driver.find_element(By.CSS_SELECTOR, ".form-select.bg-transparent"))
         selt_advertising.select_by_index(1)
 
     def adv_dropdown_platform(self):
-        drp_platform = driver.find_element(By.XPATH,
-                                           "//div[@class='col-md-3']//select[@aria-label='Default select example']")
-        drp_platform.click()
+        #drp_platform = self.driver.find_element(By.XPATH,
+        #                                   "//div[@class='col-md-3']//select[@aria-label='Default select example']")
+        #drp_platform.click()
 
-        time.sleep(1)
-        selt_platform = Select(
-            driver.find_element(By.XPATH, "//div[@class='col-md-3']//select[@aria-label='Default select example']"))
-        selt_platform.select_by_index(1)
-
-    def signup_driver(self):
-
-        SignUp.drp_regtype_click(self)
-        SignUp.drp_select_type(self)
-        SignUp.signup_names(self, 0, "Mark Trucks")
-        SignUp.signup_names(self, 1, "234534532")
-        SignUp.signup_names(self, 2, "marktrucks@yomail.com")
-        SignUp.phonenumber(self, "+1 234 454 3423")
-        SignUp.scroll_windows(self)
-        SignUp.select_address(self)
-        SignUp.btn_continue(self, 0)
-        SignUp.signup_names(self, 0, "mark")
-        SignUp.signup_names(self, 1, "mark@yopmail.com")
-        SignUp.phonenumber(self, "+1 234 454 3426")
-
-        owner_address = driver.find_element(By.CSS_SELECTOR,
-                                            ".form-control.bg-transparent.myclass-event.pac-target-input")
-        owner_address.send_keys("nj clark")
-        SignUp.signup_names(self, 2, "New York")
-        SignUp.signup_names(self, 3, "America")
-        SignUp.signup_names(self, 4, "New York")
-        SignUp.signup_names(self, 5, "10001")
-        SignUp.btn_continue(self, 1)
-        SignUp.signup_names(self, 0, "12345")
-        SignUp.signup_names(self, 1, "12345")
-        time.sleep(5)
-    def SignUp_advertiser(self):
-        SignUp.open_signup_url(self)
-        driver.implicitly_wait(4)
-        SignUp.selectplatform_advertiser(self)
-        SignUp.signup_names(self, 0, "Elon")
-        SignUp.signup_names(self, 1, "Mash")
-        SignUp.signup_names(self, 2, "Elon@yopmail.com")
-        SignUp.signup_names(self, 3, "X")
-        SignUp.signup_names(self, 4, "CEO")
-        SignUp.adv_dropdown(self, 0)
-        driver.implicitly_wait(3)
-        SignUp.adv_dropdown_platform(self)
-        SignUp.scroll_windows(self)
-        SignUp.select_address(self)
-        SignUp.btn_continue(self, 0)
-        time.sleep(3)
-        SignUp.signup_names(self, 0, "12345")
-        SignUp.signup_names(self, 1, "12345")
-        time.sleep(3)
-
-
-open_signup = SignUp()
-open_signup.SignUp_advertiser()
-
+        self.driver.find_element(By.XPATH, "//body/div[@id='main']/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[6]/div[1]/select[1]").click()
+        self.driver.find_element(By.XPATH, "//option[contains(text(),'Self')]").click()
--- TestCases/test_soMosignup.py
+++ TestCases/test_soMosignup.py
@@ -1,78 +1,73 @@
 import time
-from PageObject import SignUp
+import pytest as pytest
+from PageObject.SignUp import Signup
 from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import Select
 
-serv = Service("C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver")
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
+class TestSignup:
 
+    @pytest.fixture(scope="function")
+    def setup(self):
+        self.driver = webdriver.Firefox()
+        self.wait = WebDriverWait(self.driver, 10)
+        yield
+        self.driver.quit()
 
-class test_signup:
+    def test_signup_driver(self, setup):
 
-    def test_signup_driver(self):
-
-        SignUp.open_signup.open_signup_url()
-        SignUp.open_signup.selectplatform_driver()
-        SignUp.open_signup.drp_regtype_click(self)
-        SignUp.open_signup.drp_select_type(self)
-        SignUp.open_signup.signup_names(self, 0, "Mark Trucks")
-        SignUp.open_signup.signup_names(self, 1, "234534532")
-        SignUp.open_signup.signup_names(self, 2, "marktrucks@yomail.com")
-        SignUp.open_signup.phonenumber(self, "+1 234 454 3423")
-        SignUp.open_signup.scroll_windows(self)
-        SignUp.open_signup.select_address(self)
-        SignUp.open_signup.btn_continue(self, 0)
-        SignUp.open_signup.signup_names(self, 0, "mark")
-        SignUp.open_signup.signup_names(self, 1, "mark@yopmail.com")
-        SignUp.open_signup.phonenumber(self, "+1 234 454 3426")
-
-        owner_address = driver.find_element(By.CSS_SELECTOR,
-                                            ".form-control.bg-transparent.myclass-event.pac-target-input")
-        owner_address.send_keys("nj clark")
-        SignUp.open_signup.signup_names(self, 2, "New York")
-        SignUp.open_signup.signup_names(self, 3, "America")
-        SignUp.open_signup.signup_names(self, 4, "New York")
-        SignUp.open_signup.signup_names(self, 5, "10001")
-        SignUp.open_signup.btn_continue(self, 1)
-        SignUp.open_signup.signup_names(self, 0, "12345")
-        SignUp.open_signup.signup_names(self, 1, "12345")
-        time.sleep(5)
-        print("Success")
         print("Test case started successfully")
-        driver.close()
+        self.sp= Signup(self.driver)
+        self.sp.open_signup_url()
+        self.sp.click_on_driver_button()
+        self.sp.drp_regtype_click()
+        self.sp.drp_select_type()
+        self.sp.signup_names(0, "Mark Trucks")
+        self.sp.signup_names(1, "234534532")
+        self.sp.signup_names(2, "marktrucks@yomail.com")
+        self.sp.enter_phone_number("+1 234 454 3423")
+        self.sp.scroll_windows()
+        self.sp.select_address()
+        self.sp.click_btn_continue(0)
+        self.sp.signup_names(0, "mark")
+        self.sp.signup_names(1, "mark@yopmail.com")
+        self.sp.enter_phone_number("+1 234 454 3426")
+        self.sp.enter_owner_address("nj clark")
 
-    def test_signup_advertiser(self):
+        self.sp.signup_names(2, "New York")
+        self.sp.signup_names(3, "America")
+        self.sp.signup_names(4, "New York")
+        self.sp.signup_names(5, "10001")
+        self.sp.click_btn_continue(1)
+        self.sp.signup_names(0, "12345")
+        self.sp.signup_names(1, "12345")
+        self.sp.click_btn_continue(1)
+        self.sp.verify_success_message()
+        time.sleep(5)
+        print("Test case completed successfully")
 
-        SignUp.open_signup.open_signup_url()
-        SignUp.open_signup.selectplatform_advertiser()
-        SignUp.open_signup.signup_names(self, 0, "Elon")
-        SignUp.open_signup.signup_names(self, 1, "Mash")
-        SignUp.open_signup.signup_names(self, 2, "Elon@yopmail.com")
-        SignUp.open_signup.signup_names(self, 3, "X")
-        SignUp.open_signup.signup_names(self, 4, "CEO")
-        driver.implicitly_wait(3)
-        SignUp.open_signup.adv_dropdown_platform(self)
-        SignUp.open_signup.scroll_windows(self)
-        SignUp.open_signup.select_address(self)
-        SignUp.open_signup.btn_continue(self, 0)
-        driver.implicitly_wait(3)
-        SignUp.open_signup.signup_names(self, 0, "12345")
-        SignUp.open_signup.signup_names(self, 1, "12345")
-        time.sleep(3)
+    def test_signup_advertiser(self, setup):
 
-        print("success")
+        print("Test case started successfully")
+        self.sp= Signup(self.driver)
+        self.sp.open_signup_url()
+        self.sp.selectplatform_advertiser()
+        self.sp.signup_names(0, "Elon")
+        self.sp.signup_names(1, "Mash")
+        self.sp.signup_names(2, "Elon1@yopmail.com")
+        self.sp.signup_names(3, "XX")
+        self.sp.signup_names(4, "CEO")
+        self.sp.adv_dropdown_platform()
+        self.sp.scroll_windows()
+        self.sp.select_address()
+        self.sp.click_btn_continue(0)
+        self.sp.enter_password("12345")
+        self.sp.enter_confirm_password("12345")
+        self.sp.click_btn_continue(1)
+        self.sp.click_btn_continue(0)
+        self.sp.verify_success_message()
+        time.sleep(5)
+        print("Test case completed successfully")
 
-
-openurl = test_signup()
-
-openurl.test_signup_driver()
+if __name__ == "__main__":
+    pytest.main()
 # openurl.test_signup_advertiser()
